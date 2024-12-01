47daa740b0718304935603bb920191fd7af537d3
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-01 09:49:55 -0500

    Testing script

--- rooms/owasptop102021/7-identification-and-authentication-failures.sh
+++ rooms/owasptop102021/7-identification-and-authentication-failures.sh
@@ -41,5 +41,5 @@ curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L 
      -H "Origin: http://$target_ip:8088" \
      -H "Referer: http://$target_ip:8088/register.php" \
      -H "Content-Type: multipart/form-data; boundary=-----------------------------346104398912025331583998084524" \
-     --max-time 30 \
+    --no-buffer \
      --data-binary @${scriptpath}/7-data/nadege.txt
