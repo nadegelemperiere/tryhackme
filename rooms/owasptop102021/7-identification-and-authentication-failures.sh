b3185423176def5a680c5ae2dabec34ef45d2198
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 22:46:40 -0500

    Testing script

--- rooms/owasptop102021/7-identification-and-authentication-failures.sh
+++ rooms/owasptop102021/7-identification-and-authentication-failures.sh
@@ -37,5 +37,5 @@ echo "--> Flag is : $(sed -n ':a;N;$!ba;s/.*<p[^>]*>\(.*\)<\/p>.*/\1/p' /work/7-
 
 # Registering nadege a second time
 echo "4 - REGISTERING ARTHUR"
-curl -s -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -L \
+curl -s -X POST "http://$target_ip:8088/register.php" -b /work/7-cookies.txt -v -L \
      -H "Content-Type: multipart/form-data; boundary=-----------------------------boundary" --data-binary @${scriptpath}/7-data/nadege.txt
