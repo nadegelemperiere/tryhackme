0b34424e596ac3fb2574b40f7483b438af0efb4b
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-01 21:54:57 -0500

    Testing script

--- rooms/adventofcyber2024/day-01.sh
+++ rooms/adventofcyber2024/day-01.sh
@@ -11,11 +11,20 @@ attack_ip="10.10.190.240"
 # Prepare environment
 mkdir /work/ 2>/dev/null
 
-# Retrieving conversion
+# Retrieving conversion result
 echo "1 - RETRIEVING CONVERTED FILES"
 curl -s -X GET "http://$target_ip/download" -L -o /work/01-download.zip > /dev/null
+
+
+# Analyze files
+echo "2 - ANALYZING FILES"
 mkdir /work/01-download
 unzip -o /work/01-download.zip -d /work/01-download/
 echo "--> song.mp3 : $(file /work/01-download/song.mp3)"
 echo "--> somg.mp3 : $(file /work/01-download/somg.mp3)"
 exiftool /work/01-download/somg.mp3 > /work/10-somg.txt
+
+# Gather malware code
+echo "3 - COLLECTING MALWARE CODE"
+curl -s -X GET "https://raw.githubusercontent.com/MM-WarevilleTHM/IS/refs/heads/main/IS.ps1" -L -o /work/01-IS.ps1 > /dev/null
+
