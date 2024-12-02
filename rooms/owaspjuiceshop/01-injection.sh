1dc4de54e15cddad67f859773af608b7603ec440
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-02 09:14:41 -0500

    Testing script

--- rooms/owaspjuiceshop/01-injection.sh
+++ rooms/owaspjuiceshop/01-injection.sh
@@ -14,7 +14,7 @@ mkdir /work/ 2>/dev/null
 # Initiate session
 echo "1 - INITIATING SESSION"
 curl -s -X GET "http://$target_ip" -c "/work/1-cookies.txt" -L > /dev/null
-curl -s -X GET "http://$target_ip/socket.io/?EIO=3&transport=polling&t=PE7d5_o" -c "/work/1-cookies.txt" -L > /dev/null
+curl -s -X GET "http://$target_ip/socket.io/?EIO=3&transport=polling&t=PE7d5_o" -c "/work/1-cookies.txt" -L
 IO=$(grep io "/work/1-cookies.txt" | awk '{print $7}')
 echo "--> Session initiated with cookie IO ${IO}"
 
