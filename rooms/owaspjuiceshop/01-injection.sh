019a1491a336a900cc1d56df900c9cbd4c9c0fbe
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2024-12-02 09:17:11 -0500

    Testing script

--- rooms/owaspjuiceshop/01-injection.sh
+++ rooms/owaspjuiceshop/01-injection.sh
@@ -17,7 +17,7 @@ curl -s -X GET "http://$target_ip" -c "/work/1-cookies.txt" -L > /dev/null
 curl -s -X GET "http://$target_ip/socket.io/?EIO=3&transport=polling&t=PE7d5_o" -c "/work/1-cookies.txt" -L
 IO=$(grep io "/work/1-cookies.txt" | awk '{print $7}')
 echo "--> Session initiated with cookie IO ${IO}"
-sleep 1
+sleep 3
 curl -s -X GET "http://$target_ip/socket.io/?EIO=3&transport=polling&t=PE7d5_o" -b "/work/1-cookies.txt" -L
 
 # Authenticate using SQL injection session
